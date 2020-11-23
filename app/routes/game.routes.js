01302b7593f28ebecbb07108b5b62c2c083c7d76
rakeshcocoalabs <rakesh@cocoalabs.in>
2020-11-23 11:06:00 +0530

    added new api in games

--- app/routes/game.routes.js
+++ app/routes/game.routes.js
@@ -2,6 +2,7 @@ const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const games = require('../controllers/games.controller');
     app.get('/games',auth, games.listAllGames);
+    app.patch('/games/download',auth, games.didClickDownloadButton);
     app.get('/games/detail/:id',auth,games.getGameDetail);
     app.get('/games/summary',auth,games.getSummary);
     app.get('/games/summary/v2',auth,games.getSummaryForWeb);
