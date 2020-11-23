6524aaf4b127041fa34d2c2cb0b3506a75d6d7cd
rakeshcocoalabs <rakesh@cocoalabs.in>
2020-11-23 18:20:23 +0530

    f

--- app/routes/favouriteTutor.routes.js
+++ app/routes/favouriteTutor.routes.js
@@ -1,7 +1,7 @@
 const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const favourites = require('../controllers/favouritesTutors.controller');
-    app.get('/favouritestutors',auth, favourites.getUserFavourites);
-    app.post('/favouritetutor/addtutor',auth,favourites.addfavourite);
-    app.delete('/removetutor',auth,favourites.removeFav);
+   
+    app.post('/favouriteTutor/addtutor',auth,favourites.addfavourite);
+   
 }
