711394ee64abb46154fc7bae2ec855c8c3e9e903
rakeshcocoalabs <rakesh@cocoalabs.in>
2020-11-24 07:17:59 +0530

    f

--- app/routes/tutor.routes.js
+++ app/routes/tutor.routes.js
@@ -1,14 +1,7 @@
 const auth = require('../middleware/auth.js');
-const favouriteTutorModel = require('../models/favouriteTutor.model.js');
-function tutor(req,res){
-    console.log('54');
-    return res.send("ok")
-}
 module.exports = (app) => {
-   // const store = require('../controllers/store.controller');
-    console.log('test44');
-    app.post('/tutor', tutor);
-    
-    //store autocomplete
-   
+    const favourites = require('../controllers/favouritesTutor.controller');
+    app.get('/favourites',auth, favourites.getUserFavourites);
+    app.post('/favourites',auth,favourites.addfavourite);
+    app.delete('/favourites',auth,favourites.removeFav);
 }
