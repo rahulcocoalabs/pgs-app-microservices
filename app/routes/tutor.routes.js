557aafb4274f74d1cc3774f3637dcc8b0049f9b3
rakeshcocoalabs <docsofrakesh@gmail.com>
2020-11-24 18:14:57 +0530

    f

--- app/routes/tutor.routes.js
+++ app/routes/tutor.routes.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
     const favourites = require('../controllers/favouriteTutor.controller');
   //  app.get('/favourites',auth, favourites.getUserFavourites);
     app.post('/tutor/tutoradd',auth,favourites.addfavourite);
-    app.post('/tutor/classadd',auth,favourites.addfavouriteClass);
+    app.post('/tutor/classadd',auth,favourites.addfavourite);
     app.delete('/tutor/tutordelete/:id',auth,favourites.removefavourite);
     app.delete('/tutor/classdelete/:id',auth,favourites.removefavouriteClass);
 }
