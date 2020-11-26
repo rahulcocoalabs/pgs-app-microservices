a4e6eb364101516799ed925f399831a45188a9fc
rakeshcocoalabs <docsofrakesh@gmail.com>
2020-11-26 07:26:51 +0530

    f

--- app/routes/favourite.routes.js
+++ app/routes/favourite.routes.js
@@ -1,7 +1,13 @@
 const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const favourites = require('../controllers/favourites.controller');
-    app.get('/favourites',auth, favourites.getUserFavourites);
-    app.post('/favourites',auth,favourites.addFav);
-    app.delete('/favourites',auth,favourites.removeFav);
+    app.get('/favourites', auth, favourites.getUserFavourites);
+    app.post('/favourites', auth, favourites.addFav);
+    app.delete('/favourites', auth, favourites.removeFav);
+    const favourites = require('../controllers/favouriteTutor.controller');
+    //  app.get('/favourites',auth, favourites.getUserFavourites);
+    app.post('/favourites/tutoradd', auth, favourites.addfavourite);
+    app.post('/favourites/classadd', auth, favourites.addfavouriteClass);
+    app.delete('/favourites/tutordelete', auth, favourites.removefavourite);
+    app.delete('/favourites/classdelete', auth, favourites.removefavouriteClass);
 }
