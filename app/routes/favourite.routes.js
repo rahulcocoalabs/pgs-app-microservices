0afdd56181b6cc426be410a350b0838e0ff93a90
rakeshcocoalabs <docsofrakesh@gmail.com>
2020-11-26 09:39:28 +0530

    f

--- app/routes/favourite.routes.js
+++ app/routes/favourite.routes.js
@@ -4,10 +4,10 @@ module.exports = (app) => {
     app.get('/favourites', auth, favourites.getUserFavourites);
     app.post('/favourites', auth, favourites.addFav);
     app.delete('/favourites', auth, favourites.removeFav);
-    const favourites = require('../controllers/favouriteTutor.controller');
+    const favourites1 = require('../controllers/favouriteTutor.controller');
     //  app.get('/favourites',auth, favourites.getUserFavourites);
-    app.post('/favourites/tutoradd', auth, favourites.addfavourite);
-    app.post('/favourites/classadd', auth, favourites.addfavouriteClass);
-    app.delete('/favourites/tutordelete', auth, favourites.removefavourite);
-    app.delete('/favourites/classdelete', auth, favourites.removefavouriteClass);
+    app.post('/favourites/tutoradd', auth, favourites1.addfavourite);
+    app.post('/favourites/classadd', auth, favourites1.addfavouriteClass);
+    app.delete('/favourites/tutordelete', auth, favourites1.removefavourite);
+    app.delete('/favourites/classdelete', auth, favourites1.removefavouriteClass);
 }
