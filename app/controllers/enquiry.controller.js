ed8810a9726ae6d01508fe8fd7395e6f60383114
rakeshcocoalabs <docsofrakesh@gmail.com>
2020-12-01 16:56:51 +0530

    new api enquery

--- app/controllers/enquiry.controller.js
+++ app/controllers/enquiry.controller.js
@@ -4,3 +4,32 @@ const Enquiry = require('../models/enquiry.model.js');
     console.log("listAll() working...");
   };
 
+  exports.add = async (req,res) => {
+
+    var params = req.body;
+    var userData = req.identity.data;
+    var userId = userData.userId;
+
+    if (!params.message){
+      return res.send({
+        success:0,
+        message:"please add some message"
+      })
+    }
+    var userData = await userId.findOne({_id:userId,status:1})
+
+    var msg = new Enquiry({
+      email:userData.email,
+      phone:userData.mobile,
+      message:params.message,
+
+    })
+
+    var saved = await msg.save();
+
+    return res.send({
+      success:1,
+      message:"recieved message"
+    })
+  }
+
