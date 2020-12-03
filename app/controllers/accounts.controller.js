bbd129dbd2471972e52d722f1e3a1388af9ae6bd
rakeshcocoalabs <docsofrakesh@gmail.com>
2020-12-03 20:33:52 +0530

    m

--- app/controllers/accounts.controller.js
+++ app/controllers/accounts.controller.js
@@ -1494,7 +1494,7 @@ exports.addratingTutor = async (req, res) => {
 async function avaregeRates(type, id) {
 
   if (type == "class") {
-    var array = await Rating.find({ classId: id }).catch(error => {
+    var array = await Rating.find({ tutorId: id }).catch(error => {
       return { success: 0, message: error.message }
     })
 
@@ -1509,7 +1509,7 @@ async function avaregeRates(type, id) {
     }
     console.log(totalRates,"test")
     var avg = totalRates / array.length;
-    var update = await User.updateOne({_id:id},{avaregeRating:avg}).catch(err=>{
+    var update = await OnlineClass.updateOne({_id:id},{avaregeRating:avg}).catch(err=>{
       return {succes:0,message:err.message}
     })
     if (update && update.succes && update.succes === 1){
