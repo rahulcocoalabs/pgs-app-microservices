b7467dd50e12de25ca593ecce3a7e4658602f79a
rahulcocoalabs <rahul@cocoalabs.in>
2020-12-16 18:04:22 +0530

    change

--- app/routes/address.routes.js
+++ app/routes/address.routes.js
@@ -3,10 +3,10 @@ const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const address = require('../controllers/address.controller');
 
-   app.post('/add-address',auth,address.addAddress);
-   app.delete('/delete-address',auth,address.deleteAddress);
-   app.get('/list-address',auth,address.listAddress);
-   app.get('/list-countries',auth,address.listCountries);
-   app.get('/list-states/:countryId',auth,address.listStates);
-   app.get('/list-cities/:stateId',authaddress.listCities);
+   app.post('/address/add-address',auth,address.addAddress);
+   app.delete('/address/delete-address',auth,address.deleteAddress);
+   app.get('/address/list-address',auth,address.listAddress);
+   app.get('/address/list-countries',auth,address.listCountries);
+   app.get('/address/list-states/:countryId',auth,address.listStates);
+   app.get('/address/list-cities/:stateId',authaddress.listCities);
 }
