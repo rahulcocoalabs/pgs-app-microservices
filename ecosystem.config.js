6e07d8c8b686d22c276733ebba58ad37b51e26e8
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-06 11:26:42 +0530

    A

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -336,5 +336,19 @@ module.exports = {
         port : 7023
       }
     },
+    {
+      name: 'test - Edunet Microservices',
+      script: 'test.service.js',
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      //cron_restart
+      env: {
+        NODE_ENV: 'qa',
+        port : 7024
+      }
+    },
     ]
   };
---
+++ test.service.js
@@ -0,0 +1,4 @@
+var server = require('./server.js'); 
+var routes = ['test'];
+var serviceName = "test";
+server.start(serviceName, routes);
