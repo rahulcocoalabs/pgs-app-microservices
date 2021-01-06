62e52f2e5e8ea0814435136c0cf0b4e48587b966
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-06 14:00:24 +0530

    C

--- app/routes/trial.routes.js
+++ app/routes/trial.routes.js
@@ -3,6 +3,6 @@ const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const tests = require('../controllers/trial.controller');
     console.log("flag ..2");
-    app.post('/trial/accounts',auth, tests.create);
+    app.post('/trial/accounts', tests.create);
    
 }
