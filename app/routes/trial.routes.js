bd6298e3b43201ff8ab038a0ab085fa8b07e591f
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-08 16:35:42 +0530

    X

--- app/routes/trial.routes.js
+++ app/routes/trial.routes.js
@@ -4,6 +4,6 @@ module.exports = (app) => {
     const tests = require('../controllers/trial.controller');
     
     app.get('/trial/get-credentials',auth, tests.getCredentials);
-    app.post('/trial/update-payments',auth, tests.updatePayments);
+    app.post('/trial/update-payments',auth, tests.updatePayment);
    
 }
