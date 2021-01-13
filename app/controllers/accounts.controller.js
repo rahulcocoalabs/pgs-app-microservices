7196e58c07533b66a678fc63fdd14910a9d2b828
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-13 12:54:54 +0530

    X

--- app/controllers/accounts.controller.js
+++ app/controllers/accounts.controller.js
@@ -4738,8 +4738,8 @@ async function send_otp_bymail(email){
     var transporter = nodemailer.createTransport({
       service: 'yahoo',
       auth: {
-        user: 'rakeshkrishnanp@yahoo.com',
-        pass: 'Alpha@beta123'
+        user: 'mailrkponline@gmail.com',
+        pass: 'alifbetgimel'
       }
     });
     
