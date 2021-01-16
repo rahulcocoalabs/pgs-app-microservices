7465cbf8102109596bc377665ac294c325b0b78d
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-16 13:15:45 +0530

    X

--- app/controllers/accounts.controller.js
+++ app/controllers/accounts.controller.js
@@ -2834,7 +2834,7 @@ exports.sendOtp_1 = async (req, res) => {
     }
     else {
       var mobileNum = countryCode + mobile;
-      var otpResponse = await send_otp_bymail_1(email, mobileNum);
+      var otpResponse = await send_otp_bymail(email, mobileNum);
       if (otpResponse == undefined) {
         return res.send({
           success: 0,
