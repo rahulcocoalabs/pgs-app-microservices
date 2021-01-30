c2cbc901277b7134e4c588b94a828f4e66899769
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-30 14:06:48 +0530

    X

--- app/routes/alumni.routes.js
+++ app/routes/alumni.routes.js
@@ -2,13 +2,17 @@ var multer = require('multer');
 var crypto = require('crypto');
 var mime = require('mime-types');
 var config = require('../../config/app.config.js');
-var feedsConfig = config.feeds;
+var feedsConfig = config.alumni;
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        if (req.file.images)
-            cb(null, feedsConfig.imageUploadPath);
-      
+        if (file.fieldname === "image"){
+            cb(null, usersConfig.imageUploadPath.trim());
+        } else if (file.fieldname === "video"){
+            cb(null, tutorsConfig.videoUploadPath.trim());
+        }else{
+            return cb({success: 0, message: "Invalid types" });
+        }
     },
     filename: function (req, file, cb) {
         crypto.pseudoRandomBytes(16, function (err, raw) {
