b878be7145435aab53d81fc67592a8b54e28f87e
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-01-30 14:00:06 +0530

    X

--- app/routes/alumni.routes.js
+++ app/routes/alumni.routes.js
@@ -6,15 +6,9 @@ var feedsConfig = config.feeds;
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        if (req.files.images && !req.files.video && !req.files.documents)
+        if (req.file.images)
             cb(null, feedsConfig.imageUploadPath);
-        if (!req.files.images && req.files.video && !req.files.documents)
-            cb(null, feedsConfig.videoUploadPath);
-        if (!req.files.images && !req.files.video && req.files.documents)
-            cb(null, feedsConfig.documentUploadPath);
-        if (!(req.files.images && !req.files.video && !req.files.documents) && !(!req.files.images && req.files.video && !req.files.documents) && !(!req.files.images && !req.files.video && req.files.documents)) {
-            return cb({success: 0, message: "You cannot post multiple types" });
-        }
+      
     },
     filename: function (req, file, cb) {
         crypto.pseudoRandomBytes(16, function (err, raw) {
