0fca75d31b8234b9e0f6eb438872e20a6884d365
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-02-01 12:31:21 +0530

    X

--- app/models/alumni.model.js
+++ app/models/alumni.model.js
@@ -14,7 +14,7 @@ const AlumniSchema = mongoose.Schema({
     contact: String,
     facebook: String,
     linkedin: String,
-    status:1,
+    status:Number,
     groupName: String,
     groupTargets: String,
     createdBy:{ type: mongoose.Schema.Types.ObjectId, ref: 'User' },
