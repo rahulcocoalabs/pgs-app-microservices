289ce7f8a5db9a580fe50e32414a93256a16798d
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-02-04 00:18:42 +0530

    X

--- app/controllers/onlineClass.controller.js
+++ app/controllers/onlineClass.controller.js
@@ -374,6 +374,8 @@ async function updateClassAndSubject(classId,subjectId,userId){
     console.log("update => " , update);
     console.log("array =>" , array1);
     console.log("array 2 => " , array2);
+    console.log("class",classId);
+    console.log("subject", subjectId);
     var updateinfo = await User.updateOne({status:1, _id: userId},update).catch(err => {return 0})
 
     if (updateinfo == 0){
