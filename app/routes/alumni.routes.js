4cff6db7f39af29945b2ebf05596658e84512bf0
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-02-04 16:38:55 +0530

    X

--- app/routes/alumni.routes.js
+++ app/routes/alumni.routes.js
@@ -3,6 +3,7 @@ var crypto = require('crypto');
 var mime = require('mime-types');
 var config = require('../../config/app.config.js');
 var feedsConfig = config.alumni;
+var feedsConfigEvents = config.alumniEvents;
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -21,7 +22,25 @@ var storage = multer.diskStorage({
     }
 });
 
+var storage1 = multer.diskStorage({
+    destination: function (req, file, cb) {
+        if (file.fieldname === "image"){
+            cb(null, feedsConfigEvents.imageUploadPath.trim());
+        } else{
+            return cb({success: 0, message: "Invalid types" });
+        }
+    },
+    filename: function (req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+            if (err)
+                return cb(err)
+            cb(null, raw.toString('hex') + "." + mime.extension(file.mimetype))
+        })
+    }
+});
+
 var upload = multer({ storage: storage });
+var upload1 = multer({ storage: storage1 });
 
 const auth = require('../middleware/auth.js');
 
@@ -35,7 +54,7 @@ module.exports = (app) => {
    app.get('/alumni/list-join-request', auth,  alumni.listJoinRequests);
    app.patch('/alumni/:id/accept-join-request',auth,alumni.acceptJoinRequests)
 
-   app.post('/alumni/add-event', auth,upload.single('image'),  alumni.addAlumniEvents);
+   app.post('/alumni/add-event', auth,upload1.single('image'),  alumni.addAlumniEvents);
    app.post('/alumni/add-job', auth,upload.single('image'),  alumni.addAlumniJobs);
   
 }
