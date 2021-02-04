04148a56a218ec7e75cc1b34d5019718f7cf12f6
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-02-04 20:14:00 +0530

    ios issue

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -52,6 +52,7 @@ module.exports = (app) => {
     app.post('/accounts/reset/:token',accounts.resetPassword);
     app.get('/accounts/self',auth, accounts.getUserDetails);
     app.patch('/accounts',auth,fileUpload.single('image'),  accounts.update);
+    app.post('/accounts/update-profile-ios',auth,fileUpload.single('image'),  accounts.update);
     //app.patch('/accounts/addpassword',auth,  accounts.addPassword);
     // app.patch('/accounts',auth, fileUpload.single('image'), accounts.update);
     app.get('/accounts/get-karma',auth, accounts.getKarmaIndex);
