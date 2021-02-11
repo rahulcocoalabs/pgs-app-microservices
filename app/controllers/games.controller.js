9767b8e9c7a601d7842ca652d76b9e12b6d533d2
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-02-11 18:16:21 +0530

    X

--- app/controllers/games.controller.js
+++ app/controllers/games.controller.js
@@ -178,7 +178,7 @@ exports.getGameDetail = (req, res) => {
     categories: 1,
     image: 1,
     banners: 1,
-    playstroreUrl: 1,
+    playstroreURL: 1,
     videoYoutubeId: 1,
     appStoreUrl: 1,
     description: 1,
@@ -189,7 +189,7 @@ exports.getGameDetail = (req, res) => {
     isFav: 1
   }
   // get data
-  Game.findOne(filters).populate('categories').then(game => {
+  Game.findOne(filters,queryProjection).populate('categories').then(game => {
     if (!game) {
       var responseObj = {
         success: 0,
