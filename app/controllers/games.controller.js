de4135d1f2ea22f732f207a454fcc291052d2cae
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-02-11 17:58:09 +0530

    X

--- app/controllers/games.controller.js
+++ app/controllers/games.controller.js
@@ -253,7 +253,7 @@ exports.getGameDetail = (req, res) => {
             title: game.title || null,
             image: game.image || null,
             description: game.description || null,
-            playstoreUrl: game.playstoreUrl || null,
+            playStoreUrl: game.playstoreUrl || null,
             videoYoutubeId: game.videoYoutubeId || null,
             appStoreUrl: game.appStoreUrl || null,
             viewCount: game.viewCount || null,
