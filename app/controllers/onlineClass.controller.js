13f1dda28c3ed1d848d807cf11c33609f1168725
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-03-10 13:11:17 +0530

    X

--- app/controllers/onlineClass.controller.js
+++ app/controllers/onlineClass.controller.js
@@ -2789,7 +2789,7 @@ exports.listInstitutionClassAppointment = async(req,res) => {
     return res.send(data1);
   }
 
-  data1.subject = "chemistry";
+  data1['subject'] = "chemistry";
 
   var dataCount = await  InstituteClassAppointmentRequest.countDocuments({status:1,instituteClassId:classId}).catch(err=>{
     return {success:0,message:"something went wrong",error:err.message};
