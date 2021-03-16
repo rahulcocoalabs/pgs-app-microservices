7bcf0ad46535af143b8915c73f646958401e0421
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-03-16 07:54:48 +0530

    X

--- app/controllers/alumni.controller.js
+++ app/controllers/alumni.controller.js
@@ -233,10 +233,10 @@ exports.listAlumni1 = async (req, res) => {
         var elem1 = {};
         var elem = arr1[x];
        
-
+        elem1["isMember"] = 1;
         for (var propKey in elem)
             elem1[propKey] = elem[propKey];
-        elem1["isMember"] = 1;
+        
        
         arr3.push(elem1);
     }
