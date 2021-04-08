de5a3aefecdc4f8d4f7fc3c2dfe24202dcb31bbc
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-04-08 15:14:04 +0530

    debugging tutor listing api

--- app/controllers/onlineClass.controller.js
+++ app/controllers/onlineClass.controller.js
@@ -863,6 +863,7 @@ exports.listTutorList = async (req, res) => {
   findCriteria.isTutor = true;
   findCriteria.status = 1;
 
+  console.log(findCriteria)
 
   var listTutorResp = await listTutors(findCriteria, params.perPage, params.page, favouriteData)
   return res.send(listTutorResp);
