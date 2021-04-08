540679a07c9d9c9df6213ff119f878b57fb61278
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-04-08 15:43:43 +0530

    debug in search

--- app/controllers/masters.controller.js
+++ app/controllers/masters.controller.js
@@ -562,6 +562,7 @@ exports.getRegisterMasters = (req, res) => {
 exports.searchKeywords = async(req, res) => {
   var params = req.query;
   console.log(params.keyword);
+  console.log(params);
   var keyword = params.keyword;
   var regexp = new RegExp(keyword);
  // var filters = { value: { '$regex': regexp, '$options': 'i' }, status: 1 };
