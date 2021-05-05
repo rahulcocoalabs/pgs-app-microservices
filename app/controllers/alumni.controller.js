07a02c5585afe7d5ea827afaff1fd419dee38162
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-05 09:59:58 +0530

    X

--- app/controllers/alumni.controller.js
+++ app/controllers/alumni.controller.js
@@ -2014,7 +2014,7 @@ exports.contestPermission = async (req, res) => {
     }
 
 
-    var update = await AlumniContest.updateOne({ status: 1,contest:contest},updateObj).catch(err =>{
+    var update = await AlumniContest.updateOne({ status: 1,contest:body.contest},updateObj).catch(err =>{
 
         return {
             success:0,
