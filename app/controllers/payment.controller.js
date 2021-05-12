d5fb56a051283b938d348a04903d4c14d2f88b84
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-12 18:04:01 +0530

    X

--- app/controllers/payment.controller.js
+++ app/controllers/payment.controller.js
@@ -174,7 +174,7 @@ exports.savePayment = async (req, res) => {
     return res.status(200).send(savePayment);
   }
 
-  if (body.classId) {
+  if (params.classId && params.isPublic) {
     var obj = {}
     const monthNames = ["01", "02", "03", "04", "05", "06",
       "07", "08", "09", "10", "11", "12"];
