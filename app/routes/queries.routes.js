eddbb087267833745bb13cd3587118f77fd3ba93
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-18 10:53:40 +0530

    X

--- app/routes/queries.routes.js
+++ app/routes/queries.routes.js
@@ -4,6 +4,6 @@ module.exports = (app) => {
     const query = require('../controllers/queries.controller');
     
     app.get('/queries/get-categories',auth, query.getCategories);
-    
+    app.get('/queries/get-consultants/:id',auth, query.getConsultants);
    
 }
