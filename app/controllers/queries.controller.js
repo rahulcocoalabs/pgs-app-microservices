6a2f9eff2d5347a12e48ed171063b62c421b383b
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-19 16:13:55 +0530

    X

--- app/controllers/queries.controller.js
+++ app/controllers/queries.controller.js
@@ -213,3 +213,34 @@ exports.addAnswer = async(req,res) =>{
         message:"success"
     })
 }
+
+exports.listQueries = async(req,res)=> {
+
+
+    var params = req.query;
+    var page = params.page || 1;
+    page = page > 0 ? page : 1;
+    var perPage = Number(params.perPage) || queryConfig.resultsPerPage;
+    perPage = perPage > 0 ? perPage : queryConfig.resultsPerPage;
+    var offset = (page - 1) * perPage;
+    var pageParams = {
+        skip: offset,
+        limit: perPage
+    };
+    const id = req.params.id;
+    const queryList = await query.find({
+        _id:id,status:1,isAnswered:false
+    },{tsCreatedAt:0},pageParams).catch(err => {
+        return {
+            success:0,message:err.message
+        }
+    })
+
+    if (queryList && queryList.success != undefined && queryList.success ===0 ){
+        return res.send(queryList)
+    }
+
+    return res.send({
+        success:1,items:queryList,message:'success'
+    })
+}
