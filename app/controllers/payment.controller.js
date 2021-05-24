ed000d4c284495b2c1c3b164b7ed50b0c40bcfec
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-24 11:06:40 +0530

    X

--- app/controllers/payment.controller.js
+++ app/controllers/payment.controller.js
@@ -283,7 +283,7 @@ async function sendNotification(tutorId,studentName,amount,subject,currency,clas
 
 async function sendNotificationStudent(studentId,amount,subject,currency,classId) {
 
-  var notificationMessage =  "test message"//currency + " " + amount + " paid successfully for " + subject + " class"; 
+  var notificationMessage =  currency + " " + amount + " paid successfully for " + subject + " class"; 
   var filtersJsonArr = [{ "field": "tag", "key": "user_id", "relation": "=", "value": studentId }]
   // var metaInfo = {"type":"event","reference_id":eventData.id}
   var notificationObj2 = {
