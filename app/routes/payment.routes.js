07f3a7039e831f7e15453a57a7f80a924fea0113
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-29 12:29:40 +0530

    X

--- app/routes/payment.routes.js
+++ app/routes/payment.routes.js
@@ -2,7 +2,6 @@
 const auth = require('../middleware/auth.js');
 module.exports = (app) => {
     const payment = require('../controllers/payment.controller');
-    
     app.get('/payment/get-credentials',auth, payment.getCredentials);
     app.post('/payment/update-payments/:id',auth, payment.updatePayment);
     app.get('/payment/get-key',auth, payment.getKey);
