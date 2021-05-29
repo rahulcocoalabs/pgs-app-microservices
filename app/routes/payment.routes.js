4647111978ec7a63a01353cd5f2795ee040e7b53
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-05-29 12:28:57 +0530

    X

--- app/routes/payment.routes.js
+++ app/routes/payment.routes.js
@@ -7,6 +7,6 @@ module.exports = (app) => {
     app.post('/payment/update-payments/:id',auth, payment.updatePayment);
     app.get('/payment/get-key',auth, payment.getKey);
     app.get('/payment/test',auth, payment.tests);
-    app.post('/payment/save-transaction',auth, payment.savePayment);
+    app.post('/payment/save-transaction', payment.savePayment);
    
 }
