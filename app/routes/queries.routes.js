86d9a1206e7daaf3a9b0021f63d0d0c334126fe3
rakeshcocoalabs <rakesh@cocoalabs.in>
2021-06-04 16:46:32 +0530

    X

--- app/routes/queries.routes.js
+++ app/routes/queries.routes.js
@@ -6,8 +6,8 @@ module.exports = (app) => {
     app.get('/queries/get-categories',auth, query.getCategories);
     app.get('/queries/get-consultants/:id',auth, query.getConsultants);
     app.post('/queries/post-query/:id',auth, query.postQuery);
-    app.patch('/queries/post-answer/:id',auth, query.addAnswer);
-    app.get('/queries/list-queries/:id',auth, query.listQueries);
+    app.patch('/queries/post-answer/:id', query.addAnswer);
+    app.get('/queries/list-queries/:id', query.listQueries);
     app.get('/queries/list-chat/:id',auth, query.listChat);
     app.get('/queries/history/',auth, query.listHistory);
     app.get('/queries/consultant-details/:id',auth, query.consultantDetails);
